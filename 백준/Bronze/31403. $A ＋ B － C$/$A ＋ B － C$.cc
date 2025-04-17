b8e9a59a67935d20b4011b8bc3f6941af888e487
evanify0531 <101415061+evanify0531@users.noreply.github.com>
2025-04-17 23:06:53 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int GetDigitCount(int num){
+    if(num >=1 && num <=9) return 1;
+    else if(num >=10 && num <= 99) return 2;
+    else if(num >=100 && num <= 999) return 3;
+    else return 4;
+}
+
+int main(){
+    int a, b, c;
+    int res1, res2;
+
+    cin >> a >> b >> c;
+
+    // used to shift a by the amount of digits in b
+    int multiplier = pow(10, GetDigitCount(b));
+
+    res1 = (multiplier * a) + b - c;
+    res2 = a + b - c;
+
+    cout << res2 << "\n" << res1;
+}
