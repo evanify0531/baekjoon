2576bf6402f9c9df8f820c53491725246a30f93e
evanify0531 <101415061+evanify0531@users.noreply.github.com>
2026-03-15 23:09:40 +0900

    [Silver V] Title: 칠무해, Time: 5280 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14729. 칠무해/칠무해.cc
+++ 백준/Silver/14729. 칠무해/칠무해.cc
@@ -5,26 +5,41 @@
 
 using namespace std;
 
+const int MAX = 7;
+
 int main() {
     int n;
 
     cin >> n;
-    priority_queue<float, vector<float>, greater<float>> pq;
+    priority_queue<float, vector<float>, less<float>> pq;
 
     float score;
 
     for(int i=0; i<n; i++){
         cin >> score;
-        pq.push(score);
+        
+        if(pq.size() < MAX){
+            pq.push(score);
+        } else {
+            if(score < pq.top()){
+                pq.pop();
+                pq.push(score);
+            }
+
+        }
     }
 
     // for formatting to 3 decimal places
     cout << fixed << setprecision(3);
 
-    for(int i=0; i<7; i++){
-        cout << pq.top() << endl;
+    float lowestScores[MAX]; 
+
+    for(int i=0; i<MAX; i++){
+        lowestScores[MAX - i - 1] = pq.top();
         pq.pop();
     }
+
+    for (int i=0; i<MAX; i++) cout <<  lowestScores[i] << endl;
 
     return 0;
 }
